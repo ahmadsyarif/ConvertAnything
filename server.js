d830f7aa01b2ce9cabd964bf7bb8f25556a51636
asyarif <ahmadsyarif92@gmail.com>
2017-01-26 00:26:35 +0800

    host html from github

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const PORT = 8080;
@@ -6,37 +7,49 @@ const PORT = 8080;
 http.createServer(function(request,response){
 
     var filePath ='.' + request.url;
+    
+    //home 
     if(filePath=='./'){
-        filePath = './public/html/convert.html';
-    }
+        filePath = "convert.html";
+        githubPath = "https://raw.githubusercontent.com/ahmadsyarif/ConvertAnything/master/public/html/convert.html";
+        console.log("copying convert.html from " + githubPath + " to " + filePath);
+        
+        var file = fs.createWriteStream(filePath);
+        var request = https.get(githubPath, function(responseFromGithub){
+            console.log("response from github : " +response.statusCode);
+            if(responseFromGithub.statusCode ===200){
 
-    console.log('requested file is '+filePath);
-    var fileExtention = path.extname(filePath);
-    var contentType = 'text/html';
-    switch(fileExtention){
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-    }
-
-    fs.exists(filePath,function(exists){
-        if(exists){
-            fs.readFile(filePath,function(error,content){
-                if(error){
-                    response.writeHead(500);
-                    response.end();
-                }
-                else{
-                    response.writeHead(200,{'Content-Type':contentType});
-                    response.end(content,'utf-8');
-                }
-            });
-        }
-        else{
-            response.writeHead(404);
-            response.end();
-        }
-    }); 
+                responseFromGithub.pipe(file);
+                
+                fs.exists(filePath,function(exists){
+                    console.log("file exist");
+                    if(exists){
+                        fs.readFile(filePath,function(error,content){
+                            if(error){
+                                console.error("couldn't read file "+filePath);
+                                response.writeHead(500);
+                                response.end();
+                            }
+                            else{
+                                response.writeHead(200,{'Content-Type':"text/html"});
+                                response.end(content,'utf-8');
+                            }
+                        });
+                    }
+                    else{
+                        console.error("file not exist "+filePath);
+                        response.writeHead(404);
+                        response.end();
+                    }
+                }); 
+            }
+            else{
+                response.writeHead(404);
+                response.end();
+            }
+        }); 
+        
+    }    
 
 }).listen(PORT);
 console.log('server listening on port '+PORT);
